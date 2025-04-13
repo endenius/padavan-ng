95442e29c5db3d2cdb92f80031e672cec1f64033
nil <cryptonil@ya.ru>
2025-04-13 14:32:58 +0700

    wireguard vpn client interface: - supporting "Route to Remote LAN Subnet behind VPN Server" - supporting "Run the Script After Connected/Disconnected to VPN Server", the existing file /etc/storage/vpnc_server_script.sh is used action $1: start/stop using variables: IFNAME=$IF_NAME IF_ADDR IPLOCAL=$(echo "$IF_ADDR" | tr ',' '\n' | head -n1) IF_PRIVATE PEER_PUBLIC PEER_ENDPOINT PEER_KEEPALIVE PEER_ALLOWEDIPS

--- trunk/user/wireguard/wgc.sh
+++ trunk/user/wireguard/wgc.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+###
+
 IF_NAME="wg0"
 IF_ADDR=$(nvram get vpnc_wg_if_addr)
 IF_PRIVATE=$(nvram get vpnc_wg_if_private)
@@ -7,8 +9,9 @@ PEER_PUBLIC=$(nvram get vpnc_wg_peer_public)
 PEER_ENDPOINT=$(nvram get vpnc_wg_peer_endpoint)
 PEER_KEEPALIVE=$(nvram get vpnc_wg_peer_keepalive)
 PEER_ALLOWEDIPS=$(nvram get vpnc_wg_peer_allowedips)
+POST_SCRIPT="/etc/storage/vpnc_server_script.sh"
 
-POST_SCRIPT="/etc/storage/wireguard_client_script.sh"
+###
 
 log()
 {
@@ -19,7 +22,7 @@ log()
 
 error()
 {
-    log "$@"
+    log "error: $@"
     exit 1
 }
 
@@ -45,7 +48,7 @@ prepare_wg()
 
 setconf_wg()
 {
-    is_started || return 0
+    is_started || return 1
 
     cat > "/tmp/${IF_NAME}.conf.$$" <<EOF
 [Interface]
@@ -58,64 +61,85 @@ PersistentKeepalive = $PEER_KEEPALIVE
 AllowedIPs = $PEER_ALLOWEDIPS
 EOF
 
-    local res=$(wg setconf ${IF_NAME} "/tmp/${IF_NAME}.conf.$$" 2>&1)
+    local res=$(wg setconf $IF_NAME "/tmp/${IF_NAME}.conf.$$" 2>&1)
     rm -f "/tmp/${IF_NAME}.conf.$$"
     if ! echo $res | grep -q "error"; then
-        log "configuration $IF_NAME applied successfully, endpoint: $(wg show $IF_NAME endpoints | awk '{print $2}')"
+        log "configuration $IF_NAME applied successfully"
+        wg show $IF_NAME | grep -A 5 "peer:" | while read i; do
+            log "  $i"
+        done
     else
-        log "$res"
+        echo "$res" | while read i; do
+            log "$i"
+        done
         return 1
     fi
 }
 
 start_wg()
 {
-
     [ "$(nvram get vpnc_type)" == "3" -a "$(nvram get vpnc_enable)" == "1" ] || die "disabled"
-
     is_started && die "already started"
+
     prepare_wg
 
-    ip link add dev ${IF_NAME} type wireguard || error "cannot create $IF_NAME"
-    ip addr add ${IF_ADDR} dev ${IF_NAME}
+    ip link add dev $IF_NAME type wireguard || error "cannot create $IF_NAME"
     ip link set dev $IF_NAME mtu 1420
 
-    if ip link set ${IF_NAME} up; then
-        log "client started, interface: ${IF_NAME}, address: ${IF_ADDR}"
-    else
-        ip link del ${IF_NAME} >/dev/null 2>&1
-        die "${IF_NAME} startup failed"
-    fi
+    for i in $(echo "$IF_ADDR" | tr ',' '\n'); do
+        ip addr add $i dev $IF_NAME || log "error: cannot set $IF_NAME address $i"
+    done
+
+    local if_ip=$(ip addr show dev $IF_NAME | awk '/inet /{print $2}')
+    [ ! "$if_ip" ] && error "$IF_NAME interface address not set"
 
     setconf_wg || die
 
+    if ip link set $IF_NAME up; then
+        log "client started, interface: $IF_NAME, addresses: "$if_ip
+    else
+        error "$IF_NAME startup failed"
+    fi
+
     if [ "$(nvram get vpnc_dgw)" == "1" ]; then
         # default wg enable
-        host="$(wg show $IF_NAME endpoints | sed -n 's/.*\t\(.*\):.*/\1/p')"
-        ip route add $(ip route get $host |\
-           sed '/ via [0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/{s/^\(.* via [0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\).*/\1/}' | head -n 1)
-        ip route add 0.0.0.0/128.0.0.0 dev $IF_NAME
-        ip route add 128.0.0.0/128.0.0.0 dev $IF_NAME
+        for i in $(wg show $IF_NAME endpoints | awk -F'[\t:]' '/[0-9]\.[0-9]/{print $2}'); do
+            ip route add $(ip route get $i \
+                | sed '/ via [0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/{s/^\(.* via [0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\).*/\1/}' \
+                | head -n 1) metric 1
+        done
+        ip route add 0.0.0.0/128.0.0.0 dev $IF_NAME metric 1
+        ip route add 128.0.0.0/128.0.0.0 dev $IF_NAME metric 1
+    fi
+
+    local vpnc_rnet=$(nvram get vpnc_rnet)
+    local vpnc_rmsk=$(nvram get vpnc_rmsk)
+
+    if [ "$vpnc_rnet" -a "$vpnc_rmsk" ]; then
+        ip route add $vpnc_rnet/$vpnc_rmsk dev $IF_NAME metric 1
     fi
 }
 
 stop_wg()
 {
     if is_started; then
-        host="$(wg show $IF_NAME endpoints 2>/dev/null | sed -n 's/.*\t\(.*\):.*/\1/p')"
-        [ "$host" ] && ip route del $(ip route get $host |\
-             sed '/ via [0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/{s/^\(.* via [0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\).*/\1/}' | head -n 1) 2>/dev/null || true
-        ip link del dev $IF_NAME 2>/dev/null
+        for i in $(wg show $IF_NAME endpoints | awk -F'[\t:]' '/[0-9]\.[0-9]/{print $2}'); do
+            ip route del $(ip route get $i \
+                | sed '/ via [0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/{s/^\(.* via [0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\).*/\1/}' \
+                | head -n 1)
+        done
+        ip link set $IF_NAME down
+        ip link del dev $IF_NAME
         log "client stopped"
     fi
 }
 
 case $1 in
-    start|up)
+    start)
         start_wg
     ;;
 
-    stop|down)
+    stop)
         stop_wg
     ;;
 
@@ -125,8 +149,9 @@ case $1 in
     ;;
 esac
 
-# IF_NAME
-# IF_ADDR
+IFNAME=$IF_NAME
+# first interface address
+IPLOCAL=$(echo "$IF_ADDR" | tr ',' '\n' | head -n1)
 # IF_PRIVATE
 # PEER_PUBLIC
 # PEER_ENDPOINT
